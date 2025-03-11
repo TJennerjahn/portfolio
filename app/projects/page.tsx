200ea356a2aa4736d1a7ee8f2f448a14e939d318
Tobias Jennerjahn <tobiasjennerjahn@gmail.com>
2025-03-11 17:34:32 +0100

    added project url to project cards

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -5,6 +5,7 @@ import { FaPython } from "react-icons/fa";
 import { FaLinux } from "react-icons/fa";
 import { FaRust } from "react-icons/fa";
 import { RiSvelteLine } from "react-icons/ri";
+import Link from "next/link";
 
 export default function ProjectCards() {
   // Sample project data - replace with your actual projects
@@ -20,6 +21,7 @@ export default function ProjectCards() {
         <SiPostgresql size="2em" color="#0f1d40" />,
       ],
       image: "hero_image.png",
+      project_url: "https://increader.com",
     },
     {
       id: "II",
@@ -51,66 +53,68 @@ export default function ProjectCards() {
     <div className="w-full max-w-4xl mx-auto py-8">
       <div className="flex flex-col space-y-6">
         {projects.map((project, idx) => (
-          <div key={project.id} className="w-full">
-            <div
-              className="relative flex flex-col rounded-xl shadow-lg mx-auto"
-              style={{
-                maxWidth: "640px",
-                height: "320px",
-                backgroundColor: project.color,
-              }}
-            >
-              {/* Card Number */}
+          <Link href={project.project_url || "#"}>
+            <div key={project.id} className="w-full">
               <div
-                className={`absolute ${idx % 2 == 0 ? "right-4" : "left-4"} top-4 text-5xl font-serif z-2`}
+                className="relative flex flex-col rounded-xl shadow-lg mx-auto"
                 style={{
-                  color: project.textColor,
+                  maxWidth: "640px",
+                  height: "320px",
+                  backgroundColor: project.color,
                 }}
               >
-                {project.id}
-              </div>
-              {/* Tech Stack Icons */}
-              <div
-                className={`absolute top-8 ${idx % 2 == 0 ? "left-4" : "right-4"} grid grid-cols-2 gap-2`}
-              >
-                {project.techStack.map((icon, i) => (
-                  <div className="z-2" key={i}>
-                    {icon}
-                  </div>
-                ))}
-              </div>
-              {/* Background Image (if provided) */}
-              {project.image && (
+                {/* Card Number */}
                 <div
-                  className={`absolute ${idx % 2 == 0 ? "right-0 top-0" : "left-0 top-0"} bottom-0 overflow-hidden`}
+                  className={`absolute ${idx % 2 == 0 ? "right-4" : "left-4"} top-4 text-5xl font-serif z-2`}
+                  style={{
+                    color: project.textColor,
+                  }}
                 >
-                  <img
-                    src={`/${project.image}`}
-                    alt={`${project.title} illustration`}
-                    className="h-full rounded-xl object-cover object-left opacity-80"
-                  />
+                  {project.id}
                 </div>
-              )}
+                {/* Tech Stack Icons */}
+                <div
+                  className={`absolute top-8 ${idx % 2 == 0 ? "left-4" : "right-4"} grid grid-cols-2 gap-2`}
+                >
+                  {project.techStack.map((icon, i) => (
+                    <div className="z-2" key={i}>
+                      {icon}
+                    </div>
+                  ))}
+                </div>
+                {/* Background Image (if provided) */}
+                {project.image && (
+                  <div
+                    className={`absolute ${idx % 2 == 0 ? "right-0 top-0" : "left-0 top-0"} bottom-0 overflow-hidden`}
+                  >
+                    <img
+                      src={`/${project.image}`}
+                      alt={`${project.title} illustration`}
+                      className="h-full rounded-xl object-cover object-left opacity-80"
+                    />
+                  </div>
+                )}
 
-              {/* Project Title & Subtitle */}
-              <div
-                className={`absolute ${idx % 2 == 0 ? "left-0 text-left" : "right-0 text-right"} bottom-0 flex-1 p-6 mb-4`}
-              >
-                <h3
-                  className="text-xl font-serif"
-                  style={{ color: project.textColor }}
+                {/* Project Title & Subtitle */}
+                <div
+                  className={`absolute ${idx % 2 == 0 ? "left-0 text-left" : "right-0 text-right"} bottom-0 flex-1 p-6 mb-4`}
                 >
-                  {project.title}
-                </h3>
-                <p
-                  className="text-xs tracking-wider mt-1"
-                  style={{ color: project.textColor }}
-                >
-                  {project.subtitle}
-                </p>
+                  <h3
+                    className="text-xl font-serif"
+                    style={{ color: project.textColor }}
+                  >
+                    {project.title}
+                  </h3>
+                  <p
+                    className="text-xs tracking-wider mt-1"
+                    style={{ color: project.textColor }}
+                  >
+                    {project.subtitle}
+                  </p>
+                </div>
               </div>
             </div>
-          </div>
+          </Link>
         ))}
       </div>
     </div>
